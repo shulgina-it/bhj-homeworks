3b68e15a9cdb834a80f3a1779737d321b92366ba
Olga <shulgina.olgaa@gmail.com>
2026-05-10 00:08:43 +0300

    first commit reveal

---
+++ dom/reveal/task.js
@@ -0,0 +1,17 @@
+const reveals = Array.from(document.querySelectorAll('.reveal'));
+
+function revealOnScroll() {
+  reveals.forEach((reveal) => {
+    const rect = reveal.getBoundingClientRect();
+
+    if (rect.top < window.innerHeight && rect.bottom > 0) {
+      reveal.classList.add('reveal_active');
+    } else {
+      reveal.classList.remove('reveal_active');
+    }
+  });
+}
+
+revealOnScroll();
+
+window.addEventListener('scroll', revealOnScroll);
